530c0ed06c8052b0d7938e44ed2a2c081294cc8d
Jonah Romero <jonah.arom@gmail.com>
2024-11-24 12:13:28 -0500

    fixed memory system

--- console_hw/hdl/memory/memory_system.sv
+++ console_hw/hdl/memory/memory_system.sv
@@ -6,25 +6,27 @@ interface memory_bus();
     logic dispatch_read,
     
     logic[7:0] read_data,
-    logic finished_op,
+    logic busy,
 
     modport MEMORY_SYSTEM (
         input addr, write_data, dispatch_read, dispatch_write,
-        output read_data, finished_op
+        output read_data, busy
     );
     modport CONSUMER (
         output addr, write_data, dispatch_read, dispatch_write,
-        input read_data, finished_op
+        input read_data, busy
     );
 endinterface
 
-module mmio_enables(
+module mmio_mappings(
     input logic[31:0] addr,
     output logic ram_enable,
     output logic frame_buffer_enable,
-    output logic io_enable
+    output logic io_enable,
+    output logic[7:0] io_offset
 );
     always_comb begin
+        io_offset = addr[7:0];
         io_enable = addr[31:8] == 24'hFF_FF_FF;
         // ram is only 64kb
         frame_buffer_enable = addr[31:16] == 8'h00_00;
@@ -42,86 +44,104 @@ module memory_system (
 );
     logic[31:0] addr_latched;
     logic[7:0] data_latched;
+    logic[7:0] ram_data_out;
+    logic[7:0] counter;
+    logic we;
     // write enables
-    logic io_enable, frame_buffer_enable, ram_enable;
-    mmio_enables mmio_en(
+    logic[7:0] io_offset;
+    logic is_io_addr, is_frame_buffer_addr, is_ram_addr;
+    mmio_mappings mmio(
         .addr(addr_latched),
-        .ram_enable(ram_enable),
-        .frame_buffer_enable(frame_buffer_enable),
-        .io_enable(io_enable)
+        .ram_enable(is_ram_addr),
+        .frame_buffer_enable(is_frame_buffer_addr),
+        .io_enable(is_io_addr)
     );
 
     typedef enum {
         WAITING, READ_MEM, WRITE_MEM
     } mem_sys_state state;
 
+    assign mem_bus.busy = (state != WAITING);
     always_ff @ (posedge clk_in) begin
         if (rst_in) begin
             data_out <= 0;
-            finished_op <= 0;
+            counter <= 0;
+            we <= 0;
             state <= WAITING;
         end
         else begin
+            if (we) begin
+                we <= 0;
+            end
             case (state)
             READ_MEM: begin
-                
+                if (is_io_addr) begin
+                    case (io_offset)
+                        0: mem_bus.read_data <= io_bus.controller.joystick_x;
+                        1: mem_bus.read_data <= io_bus.controller.joystick_y;
+                        2: mem_bus.read_data <= io_bus.controller.buttons;
+                        default: mem_bus.read_data <= 0;
+                    endcase
+                    state <= WAITING;
+                end
+                else if (is_frame_buffer_addr) begin
+                    mem_bus.read_data <= 0;
+                    state <= WAITING;
+                end
+                else if (is_ram_addr) begin
+                    if (counter + 1 == 2) begin // wait 2 cycles
+                        state <= WAITING;
+                    end
+                    counter <= counter + 1;
+                end
             end
             WRITE_MEM: begin
-                
+                // all of these are 1 cycle.
+                if (is_io_addr) begin // cant write to io
+                    state <= WAITING;
+                end
+                else if (is_frame_buffer_addr) begin
+                    state <= WAITING;
+                end
+                else if (is_ram_addr) begin
+                    state <= WAITING;
+                end
             end
             WAITING: begin
+                counter <= 0;
                 addr_latched <= addr;
                 data_latched <= data_in;
                 if (dispatch_read) begin
-                    state <= WRITE_MEM;
+                    state <= READ_MEM;
                 end
                 else if (dispatch_write) begin
-                    state <= READ_MEM;
+                    state <= WRITE_MEM;
+                    we <= 1;
                 end
             end
             endcase
         end
     end
 
-    logic[15:0] write_addr;
-    logic[15:0] write_data;
-    logic[15:0] read_addr;
-    logic[15:0] blk_data_out[BLK_BUFFERS-1:0];
-    logic blk_we[BLK_BUFFERS-1:0];
-
-    always_comb begin
-        write_addr = 0;
-        write_data = 0;
-        read_addr = 0;
-        
-        for (int i = 0; i < BLK_BUFFERS; i++) begin
-            blk_data_out[i] = 0;
-            blk_we[i] = 0;
-        end
-    end
-
     // THE REST OF THIS IS INTERACTING WITH THE MEMORY
-    // io devices
-
-
-    // frame buffer
+    // frame buffer. 1 cycle write
     always_comb begin
         fb_bus.write_clk = clk_in;
         fb_bus.write_addr = addr_latched;
         fb_bus.write_data = data_latched;
-        fb_bus.write_enable = frame_buffer_enable && (state == WRITE_MEM);
+        fb_bus.write_enable = we && is_frame_buffer_addr;
     end
 
-    // internal ram
+    // internal ram. 2 cycle read, 1 cycle write
     xilinx_single_port_ram_read_first #(
         .RAM_WIDTH(8),                      // Specify RAM data width
         .RAM_DEPTH(64*1024),                      // Specify RAM depth (number of entries)
         .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
     ) ram (
         .addra(addr_latched),     // Address bus, width determined from RAM_DEPTH
-        .dina(0),       // RAM input data, width determined from RAM_WIDTH
-        .wea(0),         // Write enable
-        .douta(palette_out)      // RAM output data, width determined from RAM_WIDTH
+        .dina(data_latched),      // RAM input data, width determined from RAM_WIDTH
+        .wea(we && is_ram_addr),  // Write enable
+        .douta(ram_data_out)      // RAM output data, width determined from RAM_WIDTH
 
         .clka(clk_in),       // Clock
         .ena(1),         // RAM Enable, for additional power savings, disable port when not in use
--- console_hw/hdl/sys_io/sys_io.sv
+++ console_hw/hdl/sys_io/sys_io.sv
@@ -20,8 +20,19 @@ module sys_io(
     input logic rst_in,
     sys_io_bus io_bus,
 );
+    logic[7:0] spi_recv;
+    logic spi_recv_valid;
+
     spi_con #(.DATA_CLK_PERIOD(100)) spi(
         .clk_in, .rst_in, 
+        .data_in(0), .trigger_in(0),
+        .data_out(spi_recv), .data_valid_out(spi_recv_valid),
+
+       wire  chip_data_in, //(CIPO)
+       logic chip_clk_out, //(DCLK)
+       
+       logic chip_data_out, //(COPI)
+        .chip_sel_out() // (CS)
     );
 
 endmodule
